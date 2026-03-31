5b6e6d92b32973bc3a0a44dee154db3d66486762
YousefHassanien <79435259+YousefHassanien@users.noreply.github.com>
2026-03-31 14:44:10 -0400

    Blaster

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -44,7 +44,7 @@ public class Hero : MonoBehaviour
 
         // Reset the weapons to start _Hero with 1 blaster
         ClearWeapons();
-        weapons[0].SetType(eWeaponType.phaser);
+        weapons[0].SetType(eWeaponType.blaster);
     }
 
     void Update()
